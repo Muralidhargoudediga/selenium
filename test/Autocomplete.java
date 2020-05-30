029aeb83b56c92d493c8a3958e3e8ad15fe76e66
Muralidhargoudediga <muralidhargoud.ediga@gmail.com>
2020-05-30 14:18:54 +0530

    Changes

--- test/Autocomplete.java
+++ test/Autocomplete.java
@@ -4,13 +4,21 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.By;
 
 public class Autocomplete {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-        System.setProperty("webdriver.chrome.driver", "/Users/meaghanlewis/Downloads/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "/Users/mediga/Downloads/chromedriver");
 
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://formy-project.herokuapp.com/autocomplete");
+
+        WebElement autocomplete = driver.findElement(By.id("autocomplete"));
+        autocomplete.sendKeys("#56 Atmakur, Kurnool, AP");
+        Thread.sleep(1000);
+
+        WebElement autocompleteResult = driver.findElement(By.className("pac-item"));
+        autocompleteResult.click();
+
 
         driver.quit();
     }
