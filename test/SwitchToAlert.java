3352d171f4713658b2c1cc4d43d3d76b3c291fe1
Muralidhargoudediga <muralidhargoud.ediga@gmail.com>
2020-05-31 19:43:15 +0530

    changes

--- test/SwitchToAlert.java
+++ test/SwitchToAlert.java
@@ -5,13 +5,22 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SwitchToAlert {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-        System.setProperty("webdriver.chrome.driver", "/Users/meaghanlewis/Downloads/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "/Users/mediga/Downloads/chromedriver");
 
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://formy-project.herokuapp.com/switch-window");
+
+        WebElement alertButton = driver.findElement(By.id("alert-button"));
+        alertButton.click();
+
+        //Thread.sleep(3000);
+
+        Alert alert = driver.switchTo().alert();
+        //Thread.sleep(3000);
+        alert.accept();
 
         driver.quit();
     }
