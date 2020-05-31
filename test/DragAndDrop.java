b219e90777fcb240a28c31baa6ad408d1213c8ab
Muralidhargoudediga <muralidhargoud.ediga@gmail.com>
2020-05-31 20:01:01 +0530

    changes

--- test/DragAndDrop.java
+++ test/DragAndDrop.java
@@ -5,14 +5,20 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
 public class DragAndDrop {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         System.setProperty("webdriver.chrome.driver", "/Users/mediga/Downloads/chromedriver");
 
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://formy-project.herokuapp.com/dragdrop");
+        WebElement image = driver.findElement(By.id("image"));
+        WebElement box = driver.findElement(By.id("box"));
+        //Thread.sleep(3000);
 
+        Actions actions = new Actions(driver);
+        actions.dragAndDrop(image, box).build().perform();
+        //Thread.sleep(3000);
         driver.quit();
     }
 }
