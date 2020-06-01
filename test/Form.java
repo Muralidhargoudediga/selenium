beb18e6f66412d8498bc0f283cd9d96b7ead66a1
Muralidhargoudediga <muralidhargoud.ediga@gmail.com>
2020-06-01 11:17:04 +0530

    changes

--- test/Form.java
+++ test/Form.java
@@ -1,7 +1,12 @@
+import org.apache.http.util.Asserts;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import static org.junit.Assert.assertEquals;
 
 public class Form {
     public static void main(String[] args) {
@@ -21,6 +26,13 @@ public class Form {
         driver.findElement(By.id("datepicker")).sendKeys(Keys.RETURN);
         driver.findElement(By.cssSelector(".btn.btn-lg.btn-primary")).click();
 
+        //Confirm the behaviour of the test
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        WebElement alert = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("alert")));
+
+        String alertText = alert.getText();
+
+        assertEquals("The form was successfully submitted!", alertText);
         driver.quit();
     }
 }
