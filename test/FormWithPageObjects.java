6fc5307802701929490e9e4172f1bd6623a7962e
Muralidhargoudediga <muralidhargoud.ediga@gmail.com>
2020-06-01 11:37:34 +0530

    Initial commit

--- test/FormWithPageObjects.java
+++ test/FormWithPageObjects.java
@@ -15,43 +15,14 @@ public class FormWithPageObjects {
 
         driver.get("https://formy-project.herokuapp.com/form");
 
-        submitForm(driver);
+        FormPage formPage = new FormPage();
+        formPage.submitForm(driver);
 
-        waitForAlertBanner(driver);
+        ConfirmationPage confirmationPage = new ConfirmationPage();
+        confirmationPage.waitForAlertBanner(driver);
 
-        assertEquals("The form was successfully submitted!", getAlertBannerText(driver));
+        assertEquals("The form was successfully submitted!", confirmationPage.getAlertBannerText(driver));
 
         driver.quit();
-    }
-
-    public static void submitForm(WebDriver driver)
-    {
-        driver.findElement(By.id("first-name")).sendKeys("Muralidhar");
-
-        driver.findElement(By.id("last-name")).sendKeys("Goud");
-
-        driver.findElement(By.id("job-title")).sendKeys("SDE II");
-
-        driver.findElement(By.id("radio-button-3")).click();
-
-        driver.findElement(By.id("checkbox-1")).click();
-
-        driver.findElement(By.cssSelector("option[value='4']")).click();
-
-        driver.findElement(By.id("datepicker")).sendKeys("05/28/2019");
-        driver.findElement(By.id("datepicker")).sendKeys(Keys.RETURN);
-
-        driver.findElement(By.cssSelector(".btn.btn-lg.btn-primary")).click();
-    }
-
-    public static void waitForAlertBanner(WebDriver driver)
-    {
-        WebDriverWait wait = new WebDriverWait(driver, 10);
-        wait.until((ExpectedConditions.visibilityOfElementLocated(By.className("alert"))));
-    }
-
-    public static String getAlertBannerText(WebDriver driver)
-    {
-        return driver.findElement(By.className("alert")).getText();
     }
 }
