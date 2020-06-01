0890a6b699eac2b656360cd2bab74a0d97347839
Muralidhargoudediga <muralidhargoud.ediga@gmail.com>
2020-06-01 11:01:16 +0530

    changes

--- test/Form.java
+++ test/Form.java
@@ -11,6 +11,15 @@ public class Form {
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://formy-project.herokuapp.com/form");
+        driver.findElement(By.id("first-name")).sendKeys("Muralidhar");
+        driver.findElement(By.id("last-name")).sendKeys("Goud");
+        driver.findElement(By.id("job-title")).sendKeys("SDE II");
+        driver.findElement(By.id("radio-button-3")).click();
+        driver.findElement(By.id("checkbox-1")).click();
+        driver.findElement(By.cssSelector("option[value='4'")).click();
+        driver.findElement(By.id("datepicker")).sendKeys("05/28/2019");
+        driver.findElement(By.id("datepicker")).sendKeys(Keys.RETURN);
+        driver.findElement(By.cssSelector(".btn.btn-lg.btn-primary")).click();
 
         driver.quit();
     }
