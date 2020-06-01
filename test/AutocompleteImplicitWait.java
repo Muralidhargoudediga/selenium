ded8d81976ac1ad1035dfd65964aa8fbaa10807b
Muralidhargoudediga <muralidhargoud.ediga@gmail.com>
2020-06-01 10:28:20 +0530

    Initial commit

--- test/AutocompleteImplicitWait.java
+++ test/AutocompleteImplicitWait.java
@@ -3,8 +3,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.By;
 
-public class Autocomplete {
-    public static void main(String[] args) throws InterruptedException {
+import java.util.concurrent.TimeUnit;
+
+public class AutocompleteImplicitWait {
+    public static void main(String[] args) {
 
         System.setProperty("webdriver.chrome.driver", "/Users/mediga/Downloads/chromedriver");
 
@@ -14,7 +16,9 @@ public class Autocomplete {
 
         WebElement autocomplete = driver.findElement(By.id("autocomplete"));
         autocomplete.sendKeys("1555 Park Blvd, Palo Alto, CA");
-        Thread.sleep(1000);
+
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
 
         WebElement autocompleteResult = driver.findElement(By.className("pac-item"));
         autocompleteResult.click();
