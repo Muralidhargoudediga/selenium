bf283d67ab864082c4b95ff53af894ebc2251b7f
Muralidhargoudediga <muralidhargoud.ediga@gmail.com>
2020-06-01 10:01:34 +0530

    Initial Commit

--- test/Datepicker.java
+++ test/Datepicker.java
@@ -12,6 +12,9 @@ public class Datepicker {
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://formy-project.herokuapp.com/datepicker");
+        WebElement datePicker = driver.findElement(By.id("datepicker"));
+        datePicker.sendKeys("06/01/2020");
+        datePicker.sendKeys(Keys.RETURN);
 
         driver.quit();
     }
